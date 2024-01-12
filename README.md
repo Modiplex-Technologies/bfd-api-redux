ad1b0a92bab3ba8e704ccbf3666caff483156b1f
KubLuk24 <47034744+KubLuk24@users.noreply.github.com>
2024-01-12 23:03:47 +0100

    Changes for Discords.com API version 3

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Deprecation notice for <1.2.2
+**IMPORTANT:** `bfd-api-redux` versions 1.2.2 and below are NO LONGER SUPPORTED for interfacing with the Discords.com Bots API.
+
 ## NOTICE & INFO
 
 `bfd-api-redux` is an official bot-page API wrapper for [Discords.com](https://discords.com) (formerly botsfordiscord.com). Please direct all inqueries and support requests to [our support server.](https://discord.gg/dsl)
@@ -14,12 +17,6 @@ var bfd = require('bfd-api-redux');
 var api = new bfd('BFD_Token', 'botID');
 ```
 **NOTE:** *If you do now wish to use callbacks, then all actions must be used inside async functions with an `await`, for example:*
-```js
-async function user() {
-    console.log(await api.getUser('254287885585350666'));
-}
-user();
-```
 
 You can easily update your Bot's guild count using this function:
 ```js
@@ -31,37 +28,13 @@ api.setServers(serverCount)
 
 ### Get votes
 ```js
-api.getVotes12().then(votes => {
+api.getVotes().then(votes => {
     console.log(votes)
 })
-```
 
-### Get user
-```js
-api.getUser("254287885585350666").then(user => { //Provide a user id
-    console.log(user)
-})
-```
+// Or, if running inside an async function:
 
-### Get user bots
-```js
-api.getUserBots("254287885585350666").then(bots => { //Provide a user id
-    console.log(bots)
-})
-```
-
-### Get bot
-```js
-api.getUser("621352902656524288").then(bot => { //Provide a bot id
-    console.log(bot)
-})
-```
-
-### Get widget
-```js
-api.getWidget("621352902656524288").then(widget => { //Provide a bot id
-    console.log(widget)
-})
+await api.getVotes()
 ```
 
 ### Cheking a vote for one specific user (including structure)
--- src/main.js
+++ src/main.js
@@ -8,62 +8,23 @@ class bfd extends EventEmitter {
         this._token = apiToken;
     }
 
-    async getUser(id) {
-        return new Promise((resolve, reject) => {
-            https.get(`https://discords.com/bots/api/user/${id}`, async (res) => {
-                res.on('data', (d) => {
-                    resolve(JSON.parse(d.toString("utf-8")));
-                });
-            }).on('error', (e) => {
-                reject(e);
-            })
-        })
-    }
-
-    async getUserBots(id) {
-        return new Promise((resolve, reject) => {
-            https.get(`https://discords.com/bots/api/user/${id}/bots`, async (res) => {
-                res.on('data', (d) => {
-                    resolve(JSON.parse(d.toString("utf-8")));
-                });
-            }).on('error', (e) => {
-                reject(e);
-            })
-        })
-    }
-
-    async getBot(id) {
-        return new Promise((resolve, reject) => {
-            https.get(`https://discords.com/bots/api/bot/${id}`, async (res) => {
-                res.on('data', (d) => {
-                    resolve(JSON.parse(d.toString("utf-8")));
-                });
-            }).on('error', (e) => {
-                reject(e);
-            })
-        })
-    }
-
     async getVotes(id = this._id, token = this._token) {
-        return new Promise((resolve, reject) => {
-            reject ("This endpoint has been deprecated - please update to getVotes12()");
-        })
-    }
-
-    async getVotes12(id = this._id, token = this._token) {
         return new Promise((resolve, reject) => {
             https.get({
                 hostname: 'discords.com',
-                path: `/bots/api/bot/${id}/votes12h`,
+                path: `/bots/api/bot/${id}/getvotes`,
                 headers: {
                     Authorization: token,
                     "Content-Type": "application/json"
                 },
+                followRedirect: true,
                 parse: "json"
             }, async (res) => {
                 res.on('data', (d) => {
+                    if (res.statusCode !== 200) { resolve(); throw new Error(d);}
                     resolve(JSON.parse(d.toString("utf-8")));
                 });
+
             }).on('error', (e) => {
                 reject(e);
             })
@@ -72,9 +33,9 @@ class bfd extends EventEmitter {
 
     async checkVoteLight(userid) {
         return new Promise(async (resolve, reject) => {
-            const votes = await this.getVotes12();
-            if (Array.isArray(votes.entries)) {
-                resolve(votes.entries.some(vote => vote.userid === userid))
+            const votes = await this.getVotes();
+            if (Array.isArray(votes.votes)) {
+                resolve(votes.votes.some(vote => vote.user_id === userid))
             } else {
                 resolve(false);
             }
@@ -83,11 +44,11 @@ class bfd extends EventEmitter {
 
     async checkVote(userid) {
         return new Promise(async (resolve, reject) => {
-            const votes = await this.getVotes12();
-            if (Array.isArray(votes.entries)) {
-                let structure = { voted: votes.entries.some(vote => vote.userid === userid), votes: []};
-                votes.entries.forEach(vote => {
-                    if (vote.userid === userid) {
+            const votes = await this.getVotes();
+            if (Array.isArray(votes.votes)) {
+                let structure = { voted: votes.votes.some(vote => vote.user_id === userid), votes: []};
+                votes.votes.forEach(vote => {
+                    if (vote.user_id === userid) {
                         structure.votes.push(vote)
                     }
                 })
@@ -95,18 +56,6 @@ class bfd extends EventEmitter {
             } else {
                 resolve(false);
             }
-        })
-    }
-
-    async getWidget(id) {
-        return new Promise((resolve, reject) => {
-            https.get(`https://discords.com/bots/api/bot/${id}/widget`, async (res) => {
-                res.on('data', (d) => {
-                    resolve(d.toString("utf-8"));
-                });
-            }).on('error', (e) => {
-                reject(e);
-            })
         })
     }
 
@@ -119,7 +68,7 @@ class bfd extends EventEmitter {
             const options = {
                 hostname: 'discords.com',
                 port: 443,
-                path: `/bots/api/bot/${id}`,
+                path: `/bots/api/bot/${id}/setservers`,
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -130,39 +79,7 @@ class bfd extends EventEmitter {
             const req = https.request(options, res => {
 
                 res.on('data', d => {
-                    resolve(JSON.parse(d.toString("utf-8")));
-                })
-            })
-
-            req.on('error', error => {
-                reject(error)
-            })
-
-            req.write(data)
-            req.end()
-        })
-    }
-
-    async updateCard(enabled, content, id = this._id, token = this._token) {
-        return new Promise((resolve, reject) => {
-            const data = JSON.stringify({
-                enabled: enabled,
-                content: content
-            })
-
-            const options = {
-                hostname: 'discords.com',
-                port: 443,
-                path: `/bots/api/bot/${id}/card`,
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'authorization': token
-                }
-            }
-
-            const req = https.request(options, res => {
-                res.on('data', d => {
+                    if (res.statusCode !== 200) { resolve(); throw new Error(d);}
                     resolve(JSON.parse(d.toString("utf-8")));
                 })
             })
